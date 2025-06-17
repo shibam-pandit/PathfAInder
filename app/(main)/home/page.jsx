f9aeebd42e00258649ae39649cbd17a8fc71e32c
Shibam Pandit <shibampandit31@gmail.com>
2025-06-17 12:41:14 +0530

    Home Page Done

--- app/(main)/home/page.jsx
+++ app/(main)/home/page.jsx
@@ -3,6 +3,8 @@ import { getServerSession } from "next-auth";
 import { checkOnboardingCompleteStatus } from '@/lib/services/users.service'
 import OnboardingForm from './_component/Onboarding';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { getIndustryInsights } from '@/lib/services/industry_insights.service';
+import Dashboard from './_component/Dashboard';
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -22,9 +24,20 @@ export default async function Home() {
       return <OnboardingForm userId={userId} />
   }
 
+  const industry = await getIndustryInsights();
+  if (!industry) {
+    return (
+      <div className='min-h-screen flex items-center justify-center text-2xl font-bold'>
+        No industry insights available. Please complete your onboarding.
+      </div>
+    );
+  }
+
+  console.log('Industry insights:', industry);
+
   return (
-    <div className='min-h-screen flex items-center justify-center text-2xl font-bold'>
-      Home
+    <div>
+      <Dashboard industry={industry} />
     </div>
   );
 }
